0d49c94e286c3b74ac7400894292b4af8c7abc58
LeoGytis <leogytis@gmail.com>
2023-06-16 12:50:35 +0300

    setup is complete

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,10 @@
 let mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 
-mix.js('src/app.js', 'assets').sass('src/tailwind.scss', 'assets');
+mix
+  .js('src/app.js', 'assets')
+  .sass('src/tailwind.scss', 'assets')
+  .options({
+    processCssUrls: false,
+    postCss: [tailwindcss('tailwind.config.js')],
+  });
